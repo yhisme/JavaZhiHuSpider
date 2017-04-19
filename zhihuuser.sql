4819504e9dad86f18cd5e6a3f035c31968bb0cdf
yhisme <330514376@qq.com>
2017-04-19 18:06:06 +0800

    Add files via upload

---
+++ zhihuuser.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `zhihuuser` (
+  `name` varchar(30) DEFAULT NULL,
+  `sex` varchar(5) DEFAULT NULL,
+  `location` varchar(30) DEFAULT NULL,
+  `bussiness` varchar(30) DEFAULT NULL,
+  `employment` varchar(30) DEFAULT NULL,
+  `postion` varchar(30) DEFAULT NULL,
+  `education` varchar(30) DEFAULT NULL,
+  `education_extra` varchar(30) DEFAULT NULL,
+  `suppose` int(11) DEFAULT NULL,
+  `thanks` int(11) DEFAULT NULL,
+  `question` int(11) DEFAULT NULL,
+  `answer` int(11) DEFAULT NULL,
+  `article` int(11) DEFAULT NULL,
+  `followers` int(11) DEFAULT NULL,
+  `following` int(11) DEFAULT NULL,
+  `bewatched` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
